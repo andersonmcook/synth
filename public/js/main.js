900c981bbfc46b2d114af95ca888e312ffdc6aac
andersonmcook <andersonmcook@gmail.com>
2016-03-11 11:29:48 -0600

    basic slider synth

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,61 @@
 ;(function () {
-  console.log('in main.js')
+console.log('in main.js')
 
-  // TEST
-  const test = document.getElementById('test')
-  test.addEventListener('click', testFunc)
+// AUDIO CONTEXT
+window.AudioContext = window.AudioContext || window.webkitAudioContext
 
-  function testFunc () {
-    console.log('clicked')
-    const slider = document.getElementById('slider')
-    console.log('slider', slider.value)
-  }
-  // END TEST
+if (!AudioContext) {
+  alert("Sorry, your browser doesn't support the Web Audio APIs.")
+  throw new Error("Sorry, your browser doesn't support the Web Audio APIs. Execution Aborted.") // ABORT ALL
+}
+
+const audio = new AudioContext()
+const gain = audio.createGain()
+gain.gain.value = 0.5;
+
+// function testSound () {
+//   const osc = audio.createOscillator();
+//   osc.type = "square";
+//   osc.frequency.value = 3000;
+//   osc.connect(gain);
+// }
+// END AUDIO CONTEXT
+
+// TEST
+// const osc = audio.createOscillator()
+
+const test = document.getElementById('test')
+test.addEventListener('click', testFunc)
+
+const kill = document.getElementById('kill')
+kill.addEventListener('click', killer)
+
+const hertz = document.getElementById('hertz')
+hertz.addEventListener('input', testFunc)
+
+
+  const osc = audio.createOscillator()
+  osc.start()
+function testFunc () {
+  gain.connect(audio.destination)
+  console.log('clicked')
+  const slider = document.getElementById('slider')
+  console.log('slider', slider.value)
+  console.log('hertz', hertz.value)
+
+  osc.type = "square";
+  osc.frequency.value = hertz.value || 100
+  osc.connect(gain);
+
+
+
+}
+
+function killer () {
+  // const osc = audio.createOscillator()
+  gain.disconnect(audio.destination)
+}
+// END TEST
 
 
 
