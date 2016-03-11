2b252b7a94b8acc6e64037a4367d075a8d97b60c
andersonmcook <andersonmcook@gmail.com>
2016-03-11 12:55:23 -0600

    can adjust wave shape

--- public/js/main.js
+++ public/js/main.js
@@ -9,10 +9,16 @@ if (!AudioContext) {
   throw new Error("Sorry, your browser doesn't support the Web Audio APIs. Execution Aborted.") // ABORT ALL
 }
 
+// default
 const audio = new AudioContext()
 const gain = audio.createGain()
 gain.gain.value = 0.5;
 
+// adjust volume
+function volume () {
+  console.log('slider', slider.value)
+  gain.gain.value = slider.value
+}
 // function testSound () {
 //   const osc = audio.createOscillator();
 //   osc.type = "square";
@@ -23,6 +29,11 @@ gain.gain.value = 0.5;
 
 // TEST
 // const osc = audio.createOscillator()
+
+
+// LISTENERS
+const slider = document.getElementById('slider')
+slider.addEventListener('input', volume)
 
 const test = document.getElementById('test')
 test.addEventListener('click', testFunc)
@@ -36,36 +47,50 @@ hertz.addEventListener('input', testFunc)
 const xy = document.getElementById('xy')
 xy.addEventListener('mousemove', box)
 
+const shape = document.getElementById('shape')
+shape.addEventListener('change', shaper)
+// LISTENERS END
+
+
+function shaper () {
+  console.log(shape.value)
+  return shape.value
+}
+
 
   const osc = audio.createOscillator()
   osc.start()
+
 function testFunc () {
   gain.connect(audio.destination)
   console.log('clicked')
-  const slider = document.getElementById('slider')
-  console.log('slider', slider.value)
+  // const slider = document.getElementById('slider')
+  // console.log('slider', slider.value)
   console.log('hertz', hertz.value)
-  osc.type = "square";
+  // osc.type = "square";
+  osc.type = shaper()
 
   osc.frequency.value = hertz.value || 100
   osc.connect(gain);
 
-
-
 }
 
+  // const osc = audio.createOscillator()
+  // osc.start()
 function box (e) {
   gain.connect(audio.destination)
-  console.log(e.clientX)
-  osc.type = "square";
+  // console.log(e.clientX)
+  // osc.type = "square";
+  osc.type = shaper()
 
-  osc.frequency.value = e.clientX
+  osc.frequency.value = e.clientX / 3
   osc.connect(gain);
 }
 
 
 function killer () {
   // const osc = audio.createOscillator()
+  osc.frequency.value = 0
   gain.disconnect(audio.destination)
 }
 // END TEST
