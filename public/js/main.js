2041d8e3254fb39fea96c1070b081840928282fe
andersonmcook <andersonmcook@gmail.com>
2016-03-14 14:52:03 -0500

    some forward progress on keys

--- public/js/main.js
+++ public/js/main.js
@@ -31,6 +31,7 @@
   const audio = new AudioContext()
   const gain = audio.createGain()
   gain.gain.value = 0.5;
+  gain.connect(audio.destination)
 
   // adjust volume
   function volume () {
@@ -54,17 +55,17 @@
   const slider = document.getElementById('slider')
   slider.addEventListener('input', volume)
 
-  const test = document.getElementById('test')
-  test.addEventListener('click', testFunc)
+  // const test = document.getElementById('test')
+  // test.addEventListener('click', testFunc)
 
   const kill = document.getElementById('kill')
   kill.addEventListener('mouseover', killer)
 
-  const hertz = document.getElementById('hertz')
-  hertz.addEventListener('input', testFunc)
+  // const hertz = document.getElementById('hertz')
+  // hertz.addEventListener('input', testFunc)
 
-  const xy = document.getElementById('xy')
-  xy.addEventListener('mousemove', box)
+  // const xy = document.getElementById('xy')
+  // xy.addEventListener('mousemove', box)
 
   const shape = document.getElementById('shape')
   shape.addEventListener('change', getter(shape))
@@ -85,38 +86,39 @@
   }
 
 
-    const osc = audio.createOscillator()
-    osc.start()
-
-  function testFunc (sound) {
-    gain.connect(audio.destination)
-    console.log('clicked')
-    // const slider = document.getElementById('slider')
-    // console.log('slider', slider.value)
-    console.log('hertz', hertz.value)
-    // osc.type = "square";
-    // osc.type = shaper()
-    const gotHertz = getter(hertz)
-
-    osc.type = getter(shape)
-
-    // osc.frequency.value = hertz.value || 100
-    osc.frequency.value = gotHertz || 100
-    // osc.frequency.value = sound.frequency || gotHertz
-    osc.connect(gain);
-    hertzText.innerHTML = gotHertz
-    // hertzText.innerHTML = sound.frequency || gotHertz
-    const soundToSend = {frequency: gotHertz, shape: getter(shape)}
-    console.log('testFunc soundToSend', soundToSend)
-    // if (!sound) {
-      ws.emit('sendSound', soundToSend)
-    // }
-  }
-
-  function receiveSound (sound) {
-    gain.connect(audio.destination)
     // const osc = audio.createOscillator()
     // osc.start()
+
+  // function testFunc (sound) {
+  //   gain.connect(audio.destination)
+  //   console.log('clicked')
+  //   // const slider = document.getElementById('slider')
+  //   // console.log('slider', slider.value)
+  //   console.log('hertz', hertz.value)
+  //   // osc.type = "square";
+  //   // osc.type = shaper()
+  //   const gotHertz = getter(hertz)
+
+  //   osc.type = getter(shape)
+
+  //   // osc.frequency.value = hertz.value || 100
+  //   osc.frequency.value = gotHertz || 100
+  //   // osc.frequency.value = sound.frequency || gotHertz
+  //   osc.connect(gain);
+  //   hertzText.innerHTML = gotHertz
+  //   // hertzText.innerHTML = sound.frequency || gotHertz
+  //   const soundToSend = {frequency: gotHertz, shape: getter(shape)}
+  //   console.log('testFunc soundToSend', soundToSend)
+  //   // if (!sound) {
+  //     ws.emit('sendSound', soundToSend)
+  //   // }
+  // }
+
+  function receiveSound (sound) {
+    console.log('received', sound)
+    gain.connect(audio.destination)
+    const osc = audio.createOscillator()
+    osc.start()
     // console.log('clicked')
     // const slider = document.getElementById('slider')
     // console.log('slider', slider.value)
@@ -162,5 +164,260 @@
   // END TEST
 
 
+// KEYS
+
+const frequencyByKey = {
+  65: 261.626, // C4
+  87: 277.183, // C#4
+  83: 293.665, // D4
+  69: 311.127, // D#4
+  68: 329.628, // E4
+  70: 349.228, // F4
+  84: 369.994, // F#4
+  71: 391.995, // G4
+  89: 415.305, // G#4
+  72: 440.000, // A4
+  85: 466.164, // A#4
+  74: 493.883, // B4
+  75: 523.251, // C5
+  79: 554.365, // C#5
+  76: 587.330, // D5
+  80: 622.254 // D#5
+};
+
+const nameByKey = {
+  65: "C4",
+  87: "C#4",
+  83: "D4",
+  69: "D#4",
+  68: "E4",
+  70: "F4",
+  84: "F#4",
+  71: "G4",
+  89: "G#4",
+  72: "A4",
+  85: "A#4",
+  74: "B4",
+  75: "C5",
+  79: "C#5",
+  76: "D5",
+  80: "D#5"
+};
+
+let nodes = [];
+
+// you need to think about when someone is chatting that this is not triggered
+// keydown press
+function pressKey (event) {
+  console.log('in press key')
+  console.log(event.keyCode)
+  let alreadyPressed = false;
+  for (let i = 0; i < nodes.length; i++) {
+    if (nodes[i].code === event.keyCode) {
+      alreadyPressed = true
+      break
+    }
+  }
+  if (event.keyCode >= 65 && event.keyCode <= 90 && !alreadyPressed) {
+    const osc = audio.createOscillator()
+    osc.type = getter(shape)
+    osc.frequency.value = frequencyByKey[event.keyCode]
+    osc.connect(gain)
+    osc.start(0)
+    nodes.push({code: event.keyCode, node: osc})
+    const soundToSend = {frequency: frequencyByKey[event.keyCode], shape: getter(shape)}
+    ws.emit('sendSound', soundToSend)
+    // try {
+    //   osc.start(0)
+    //   nodes.push({code: event.keyCode, node: osc})
+    //   console.log(nodes)
+    //   console.log('in catch')
+    // } catch (e) {
+    //   nodes.push({code: event.keyCode, node: osc})
+    //   console.log(nodes)
+    //   console.log('in catch')
+    // //   for (let i = 0; i < nodes.length; i++) {
+    // //   str += nameByKey[nodes[i].code] + " ";
+    // // }
+    // }
+  }
+}
+
+// keydown function
+document.addEventListener('keydown', pressKey)
+
+// document.addEventListener('keydown', function (event) {
+//   console.log('keydown')
+//   var alreadyPressed = false;
+//   for (var i = 0; i < nodes.length; i++) {
+//     if (nodes[i].code === event.keyCode) {
+//       alreadyPressed = true;
+//       break;
+//     }
+//   }
+//   if (event.keyCode >= 65 && event.keyCode <= 90 && !alreadyPressed) {
+//     var osc = gain.createOscillator();
+//     osc.type = "square";
+//     osc.frequency.value = frequencyByKey[event.keyCode];
+//     osc.connect(gain);
+//     try {
+//       osc.start(0);
+//     } catch (e) {}
+//     nodes.push({
+//       code: event.keyCode,
+//       node: osc
+//     });
+
+//     // var str = "";
+//     // for (var i = 0; i < nodes.length; i++) {
+//     //   str += nameByKey[nodes[i].code] + " ";
+//     // }
+//     // show.textContent = str;
+//   }
+// }, false);
+
+function releaseKey (event) {
+  console.log('released key')
+  const garbage = []
+  for (let i = 0; i < nodes.length; i++) {
+    if (nodes[i].code === event.keyCode) {
+      nodes[i].node.stop(0)
+      nodes[i].node.disconnect()
+      garbage.push(i)
+    }
+  }
+  for (let i = 0; i < garbage.length; i++) {
+    nodes.splice(garbage[i], 1)
+  }
+}
+
+document.addEventListener('keyup', releaseKey)
+
+// document.addEventListener('keyup', function (event) {
+//   console.log('keyup')
+//   var garbage = [];
+//   for (var i = 0; i < nodes.length; i++) {
+//     if (nodes[i].code === event.keyCode) {
+//       nodes[i].node.stop(0);
+//       nodes[i].node.disconnect();
+//       garbage.push(i);
+//     }
+//   }
+//   for (var i = 0; i < garbage.length; i++) {
+//     nodes.splice(garbage[i], 1);
+//   }
+//   // var str = "";
+//   // for (var i = 0; i < nodes.length; i++) {
+//   //   str += nameByKey[nodes[i].code] + " ";
+//   // }
+//   // show.textContent = str;
+// }, false);
+
+
+// END KEYS
+
+
+
+// // WEB AUDIO API CHECK
+// window.AudioContext = window.AudioContext || window.webkitAudioContext;
+// if (!AudioContext) {
+//   alert("Sorry, your browser doesn't support the Web Audio APIs.");
+//   throw new Error("Sorry, your browser doesn't support the Web Audio APIs. Execution Aborted."); // ABORT ALL
+// }
+
+// var frequencyByKey = {
+//   65: 261.626, // C4
+//   87: 277.183, // C#4
+//   83: 293.665, // D4
+//   69: 311.127, // D#4
+//   68: 329.628, // E4
+//   70: 349.228, // F4
+//   84: 369.994, // F#4
+//   71: 391.995, // G4
+//   89: 415.305, // G#4
+//   72: 440.000, // A4
+//   85: 466.164, // A#4
+//   74: 493.883, // B4
+//   75: 523.251, // C5
+//   79: 554.365, // C#5
+//   76: 587.330, // D5
+//   80: 622.254 // D#5
+// };
+
+// var nameByKey = {
+//   65: "C4",
+//   87: "C#4",
+//   83: "D4",
+//   69: "D#4",
+//   68: "E4",
+//   70: "F4",
+//   84: "F#4",
+//   71: "G4",
+//   89: "G#4",
+//   72: "A4",
+//   85: "A#4",
+//   74: "B4",
+//   75: "C5",
+//   79: "C#5",
+//   76: "D5",
+//   80: "D#5"
+// };
+
+// var show = document.getElementById("show");
+
+// var context = new AudioContext(),
+//     gain = context.createGain(),
+//     nodes = [];
+
+// gain.gain.value = 0.5;
+// gain.connect(context.destination);
+
+// document.addEventListener('keydown', function (event) {
+//   var alreadyPressed = false;
+//   for (var i = 0; i < nodes.length; i++) {
+//     if (nodes[i].code === event.keyCode) {
+//       alreadyPressed = true;
+//       break;
+//     }
+//   }
+//   if (event.keyCode >= 65 && event.keyCode <= 90 && !alreadyPressed) {
+//     var osc = context.createOscillator();
+//     osc.type = "square";
+//     osc.frequency.value = frequencyByKey[event.keyCode];
+//     osc.connect(gain);
+//     try {
+//       osc.start(0);
+//     } catch (e) {}
+//     nodes.push({
+//       code: event.keyCode,
+//       node: osc
+//     });
+
+//     // var str = "";
+//     // for (var i = 0; i < nodes.length; i++) {
+//     //   str += nameByKey[nodes[i].code] + " ";
+//     // }
+//     // show.textContent = str;
+//   }
+// }, false);
+
+// document.addEventListener('keyup', function (event) {
+//   var garbage = [];
+//   for (var i = 0; i < nodes.length; i++) {
+//     if (nodes[i].code === event.keyCode) {
+//       nodes[i].node.stop(0);
+//       nodes[i].node.disconnect();
+//       garbage.push(i);
+//     }
+//   }
+//   for (var i = 0; i < garbage.length; i++) {
+//     nodes.splice(garbage[i], 1);
+//   }
+//   var str = "";
+//   // for (var i = 0; i < nodes.length; i++) {
+//   //   str += nameByKey[nodes[i].code] + " ";
+//   // }
+//   // show.textContent = str;
+// }, false);
 
 })();
